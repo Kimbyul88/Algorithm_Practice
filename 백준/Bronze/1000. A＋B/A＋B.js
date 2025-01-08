d37351e43c143a4651d08600bfa1f22323b153f3
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2025-01-08 10:09:46 +0900

    [Bronze V] Title: A+B, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.js
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -1,3 +1,4 @@
-const input =require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split(" ").map(Number);
+const input =require('fs').readFileSync("/dev/stdin")
+.toString().trim().split(" ").map(Number);
 
 console.log(input[0]+input[1]);
