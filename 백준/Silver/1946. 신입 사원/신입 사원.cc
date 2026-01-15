3e8a9e2ef5cc9e35a17503c0d1bd9dba05f0c69a
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2026-01-15 20:42:30 +0900

    [Silver I] Title: 신입 사원, Time: 316 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.cc
+++ 백준/Silver/1946. 신입 사원/신입 사원.cc
@@ -1,10 +1,11 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;;
 
 int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
     int t;
     cin >> t;
     
