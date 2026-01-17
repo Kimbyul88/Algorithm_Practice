0371c60946a9f9e9a09dbf11bc47a9afd7801f00
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2026-01-17 23:48:17 +0900

    [Silver V] Title: 돌 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    if (n % 2 == 1) cout << "SK";
+    else cout << "CY";
+    return 0;
+}
