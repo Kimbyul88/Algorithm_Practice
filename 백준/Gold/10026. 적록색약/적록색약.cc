4ee07b319c95c8ea3ad818ce6a52b320dbaa3887
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2026-01-30 15:40:13 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -7,11 +7,51 @@ using namespace std;
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 
+int n;
+
+int bfs(vector<vector<char>> &b){
+    vector<vector<int>> visit(n, vector<int>(n, 0));
+    int count = 0; 
+    
+    queue<pair<int,int>> Q;
+    //현재 컬러
+    int color;
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            //시작점을 찾는다. 방문한 적 있으면 패쓰
+            if(visit[i][j]) continue;
+            
+            //시작
+            Q.push({i,j});
+            visit[i][j] = 1;
+            //컬러 카운트 추가
+            count++;
+            
+            color = b[i][j];
+            while(!Q.empty()){
+                pair<int,int> cur = Q.front();Q.pop();
+                for(int dir=0;dir<4;dir++){
+                    int nx = cur.X + dx[dir];
+                    int ny = cur.Y + dy[dir];
+                    //인덱스 범위 벗어남
+                    if(nx<0||ny<0||nx>n-1||ny>n-1) continue;
+                    //문제 조건에서 벗어남
+                    if(visit[nx][ny]||b[nx][ny]!=color) continue;
+                    Q.push({nx, ny});
+                    visit[nx][ny] = 1;
+                }
+            }
+        }
+    }
+    
+    return count;
+}
+
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n;
     cin >> n;
     
     vector<vector<char>> board(n, vector<char>(n));
@@ -28,72 +68,7 @@ int main(void){
             else board2[i][j] = c;
         }
     }
-    //적록색약 있음
-    int yCount = 0;
-    //적록색약 없음
-    int nCount = 0;
-    queue<pair<int,int>> Q;
     
-    //현재 컬러
-    int color;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            //시작점을 찾는다. 방문한 적 있으면 패쓰
-            if(visit[i][j]) continue;
-            
-            //시작
-            Q.push({i,j});
-            visit[i][j] = 1;
-            //컬러 카운트 추가
-            nCount++;
-            
-            color = board[i][j];
-            while(!Q.empty()){
-                pair<int,int> cur = Q.front();Q.pop();
-                for(int dir=0;dir<4;dir++){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    //인덱스 범위 벗어남
-                    if(nx<0||ny<0||nx>n-1||ny>n-1) continue;
-                    //문제 조건에서 벗어남
-                    if(!board[nx][ny]||visit[nx][ny]||board[nx][ny]!=color) continue;
-                    Q.push({nx, ny});
-                    visit[nx][ny] = 1;
-                }
-            }
-        }
-    }
-    
-    visit.assign(n, vector<int>(n, 0));
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            //시작점을 찾는다. 방문한 적 있으면 패쓰
-            if(visit[i][j]) continue;
-            
-            //시작
-            Q.push({i,j});
-            visit[i][j] = 1;
-            //컬러 카운트 추가
-            yCount++;
-            
-            color = board2[i][j];
-            while(!Q.empty()){
-                pair<int,int> cur = Q.front();Q.pop();
-                for(int dir=0;dir<4;dir++){
-                    int nx = cur.X + dx[dir];
-                    int ny = cur.Y + dy[dir];
-                    //인덱스 범위 벗어남
-                    if(nx<0||ny<0||nx>n-1||ny>n-1) continue;
-                    //문제 조건에서 벗어남
-                    if(!board2[nx][ny]||visit[nx][ny]||board2[nx][ny]!=color) continue;
-                    Q.push({nx, ny});
-                    visit[nx][ny] = 1;
-                }
-            }
-        }
-    }
-    
-    cout << nCount << " " << yCount;
+    cout << bfs(board) << " " << bfs(board2);
     return 0;
 }
