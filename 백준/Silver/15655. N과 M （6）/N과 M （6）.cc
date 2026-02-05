4542a9b6f8171651207166cbf7415657f1425c60
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2026-02-05 09:53:36 +0900

    [Silver III] Title: N과 M (6), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.cc
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, m;
+
+int arr[10];
+int input[10];
+
+bool issued[10];
+
+// vector<int> input(10, 0);
+
+void func(int k, int start){
+    //m개를 만족하면 종료하고 프린트
+    if(k==m+1){
+        for(int i=1;i<=m;i++){
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    
+    //루프를 통해 재귀.
+    for(int st=start;st<=n;st++){
+        //if(!issued[st]){
+            arr[k] = input[st];
+            //issued[st] = 1;
+            func(k+1, st+1);
+            //issued[st] = 0;
+        //}
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n >> m;
+    
+    for(int i=1;i<=n;i++){
+        cin >> input[i];
+    }
+    
+    sort(input+1, input+n+1);
+    
+    //cout << "\n" << "input: ";
+    //for(int i : input) cout << i << " ";
+    //cout << "\n";
+    
+    func(1,1);
+    
+    return 0;
+}
