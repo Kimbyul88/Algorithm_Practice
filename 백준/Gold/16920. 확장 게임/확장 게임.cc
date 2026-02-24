5118d264c77f76bb04aa25c89e9d7b2ccb083966
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2026-02-24 22:24:28 +0900

    [Gold II] Title: 확장 게임, Time: 44 ms, Memory: 15488 KB -BaekjoonHub

---
+++ 백준/Gold/16920. 확장 게임/확장 게임.cc
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+struct Node{
+    int x,y,step;
+};
+
+bool isQlistEmpty(vector<deque<Node>>& ql){
+    //끝나는 조건 = 큐리스트의 모든 큐가 비었을때..
+    // = 큐리스트의 큐의 사이즈의 합이 0일때..
+    // OR 그냥 쭉 empty인지 확인하고 아니면 반환
+    
+    for(deque<Node> q:ql){
+        if(!q.empty()){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int n, m, p;
+    
+    cin >> n >> m >> p;
+    
+    vector<int> s(p+1,0);
+    for(int i=1;i<=p;i++) cin >> s[i];
+    
+    //p+1만큼 큐리스트를 만든다. 
+    vector<deque<Node>> qlist(p+1);
+    vector<vector<char>> board(n, vector<char>(m,0));
+    vector<int> pcount(p+1, 0);
+  
+    // . = 빈칸 , # = 벽
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+        char in;
+            cin >> in;
+            //p인덱스의 큐에 시작점들을 넣음
+            if(in!='#' && in!='.'){
+                 qlist[in-'0'].push_back({i,j,0});
+                 pcount[in-'0']++;
+            }
+            board[i][j] = in;
+        }
+    }
+
+    for(int i=1;i<=n*m;i++){
+        //큐리스트 비었는지 확인
+        if(isQlistEmpty(qlist)) break;
+        
+        //현재 플레이어 번호
+        int np = (i-1) % p +1;
+        //현재 몇회차?
+        int rd = (i-1) / p + 1;
+        int step = s[np];
+        
+        while(!qlist[np].empty()){
+            Node cur = qlist[np].front(); 
+            //***
+            if(cur.step == rd * s[np]) break;
+            //***
+            qlist[np].pop_front();
+            for(int dir=0;dir<4;dir++){
+                int nx = cur.x + dx[dir];
+                int ny = cur.y + dy[dir];
+                //보드밖
+                if(nx<0||ny<0||nx>=n||ny>=m) continue;
+                // 해당 칸이 '.'이 아니다.
+                if(board[nx][ny]!='.') continue;
+                //보드 바꾸기
+                board[nx][ny] = np + '0';
+                qlist[np].push_back({nx,ny,cur.step+1});
+                pcount[np]++;
+            }
+        }
+    }
+  
+    for(int i=1;i<=p;i++) cout << pcount[i] << " ";
+    
+    return 0;
+}
