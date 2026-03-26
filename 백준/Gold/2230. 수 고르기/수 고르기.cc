c92d1a72305745148a716cf5d4866b641f28c0d7
Kim Byul <119372886+Kimbyul88@users.noreply.github.com>
2026-03-26 23:33:57 +0900

    [Gold V] Title: 수 고르기, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.cc
+++ 백준/Gold/2230. 수 고르기/수 고르기.cc
@@ -3,34 +3,36 @@ using namespace std;
 
 int n,m;
 
-
 int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
     cin >> n >> m;
-    vector<int> A(n);
-    for(int i=0;i<n;i++) cin >> A[i];
     
-    sort(A.begin(),A.end());
-    int start = 0;
-    int end = A.size() - 1;
+    vector<int> arr(n);
     
-    deque<int> Q;
+    for(int i=0;i<n;i++) cin >> arr[i];
+    
+    int st = 0, en = 0;
+    
+    int min_v = INT_MAX;
+    
+    sort(arr.begin(), arr.end());
     
     for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int minus = A[j] - A[i];
-            if(minus>=m){
-                Q.push_back(minus);
+        st = i;
+        
+        while(en!=n){
+            if(arr[en]-arr[st]>=m){
+                min_v = min(min_v, arr[en]-arr[st]);
                 break;
+            }else{
+                en++;
             }
         }
     }
     
-    cout << *min_element(Q.begin(), Q.end());
-    // 1 3 4 6 7 , m = 3
+    cout << min_v;
     
     return 0;
-    
 }
